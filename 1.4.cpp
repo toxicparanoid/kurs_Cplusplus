1e71643e855e95f01e147748c29d40e6fc219081
toxicparanoid <Nikiboy2005@yandex.ru>
2024-12-02 23:01:46 +0700

    второе задание 4 дз

--- 1.4.cpp
+++ 1.4.cpp
@@ -1,47 +1,19 @@
-#include<iostream>
+#include <iostream>
 
 int main() {
-    setlocale(LC_ALL, "rus");
-
-    int a, b, c, d, e;
+    setlocale(LC_ALL, "RUS");
+    int number;
+    int sum = 0;
 
     std::cout << "Введите целое число: ";
-    std::cin >> a;
-    while(true){
+    std::cin >> number;
 
-        if (a == 0) {
-            std::cout << "До свидания!" << std::endl;
-          break;
-        }
-
-        if (a > 999) {
-            b = a / 1000;
-            c = a / 100 % 10;
-            d = a / 10 % 10;
-            e = a % 10;
-            std:: cout << "Сумма цифр: " << (b + c + d + e) << std::endl;
-            break;
-        }
-
-        if (a > 99) {
-            b = a / 100;
-            c = a / 10 % 10;
-            d = a % 10;
-            std:: cout << "Сумма цифр: " << (b + c + d) << std::endl;
-            break;
-        }
-
-        if (a > 9) {
-            b = a / 10;
-            c = a % 10;
-            std:: cout << "Сумма цифр: " << (b + c) << std::endl;
-            break;
-        }
-
-        if (a < 10) {
-            std::cout << "Сумма цифр: " << a << std::endl;
-            break;
-        }
+    while (number > 0) {
+        sum += number % 10;
+        number /= 10; 
     }
+
+    std::cout << "Сумма цифр: " << sum << std::endl;
+    
     return 0;
 }
