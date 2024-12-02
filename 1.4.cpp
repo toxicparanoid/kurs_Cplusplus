8726dc7a9c5fb2b46a5cd1a6d5b5601063154a73
toxicparanoid <Nikiboy2005@yandex.ru>
2024-12-02 23:27:20 +0700

    добавил 3 задание 4 дз

--- 1.4.cpp
+++ 1.4.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
+//----------------------------------------------------------
+    std::cout << "2 Задание: " << std::endl;
+    
     setlocale(LC_ALL, "RUS");
     int number;
     int sum = 0;
@@ -13,7 +16,24 @@ int main() {
         number /= 10; 
     }
 
-    std::cout << "Сумма цифр: " << sum << std::endl;
-    
+    std::cout << "Сумма цифр: " << sum << std::endl << std::endl;
+
+//----------------------------------------------------------
+    std::cout << "3 Задание: " << std::endl;
+
+    int value, ans;
+
+    std::cout << "Введите целое число: ";
+    std::cin >> value;
+
+    for (int i = 1; i < 11; i++) {
+        ans = value * i;
+        std::cout << value << " x " << i << " = " << ans << std::endl;
+    }
+
+
+
+
+
     return 0;
 }
