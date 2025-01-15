159f6f99657c8d7d791efaaeac5e936090da3271
toxicparanoid <Nikiboy2005@yandex.ru>
2025-01-16 00:32:32 +0700

    1 задание правка

--- 1.11/1.cpp
+++ 1.11/1.cpp
@@ -11,7 +11,7 @@ int main() {
     cin >> name;
     cout << "Введите фамилию: ";
     cin >> surname;
-    cout << "Здарова, " << name  << " " << surname  << "!\n";
+    cout << "Здарова, " << name + " " + surname  << "!\n";
 
     return 0;
 }
