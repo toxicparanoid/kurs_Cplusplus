69ebf07a3508f6afaa389ecb176e20a15686e07a
toxicparanoid <Nikiboy2005@yandex.ru>
2025-04-02 21:34:48 +0700

    1 дз

---
+++ 3 блок/1 дз/1.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int count(int* arr, int size, int s) {
+    int left = 0;
+    int right = size;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] > s) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return size - left;
+}
+
+int main() {
+
+    int s, size = 9;
+    int arr[size] = {14, 16, 19, 32, 32, 32, 56, 69, 72};
+
+    cout << "Введите точку отсчета: ";
+    cin >> s;
+
+    cout << "Количество элементов в массиве больших, чем " << s << ": " << count(arr, size, s) << endl;
+
+    return 0;
+}
