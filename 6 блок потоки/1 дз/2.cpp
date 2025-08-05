9f254e8ca8a92ef1d9de5cd67b0bf95f9e408050
toxicparanoid <Nikiboy2005@yandex.ru>
2025-08-05 16:55:17 +0700

    1 дз доработка

--- 6 блок потоки/1 дз/2.cpp
+++ 6 блок потоки/1 дз/2.cpp
@@ -2,31 +2,39 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <omp.h>
 #include <iomanip>
+
 using namespace std;
 
-void vectorAdd(vector<double> a, vector<double> b, vector<double> c, int threads) {
-    #pragma omp parallel num_threads(threads)
-    {
-        #pragma omp for
-        for (auto i = 0; i < a.size(); i++) {
-            c[i] = a[i] + b[i];
-        }
-
+void vectorAddPart(const vector<double>& a, const vector<double>& b, vector<double>& c, size_t start, size_t end) {
+    for (size_t i = start; i < end; ++i) {
+        c[i] = a[i] + b[i];
     }
-
-
 }
 
-int main()
-{
+void vectorAdd(const vector<double>& a, const vector<double>& b, vector<double>& c, int threads) {
+    vector<thread> thread_pool;
+    size_t size = a.size();
+    size_t chunk_size = size / threads;
+
+    for (int i = 0; i < threads; ++i) {
+        size_t start = i * chunk_size;
+        size_t end = (i == threads - 1) ? size : start + chunk_size;
+        thread_pool.emplace_back(vectorAddPart, ref(a), ref(b), ref(c), start, end);
+    }
+
+    for (auto& t : thread_pool) {
+        t.join();
+    }
+}
+
+int main() {
     setlocale(LC_ALL, "rus");
 
     int sizes[] = { 1000, 10000, 100000, 1000000 };
     int threads[] = { 2, 4, 8, 16 };
 
-    int cores = omp_get_num_procs();
+    int cores = thread::hardware_concurrency();
     cout << "Количество аппаратных ядер - " << cores << endl;
 
     cout << "Количество потоков\t" << "1000" << "\t" << "10000" << "\t"
@@ -34,8 +42,8 @@ int main()
 
     for (int t : threads) {
         cout << t << " потоков\t";
-        for (int size : sizes) {
 
+        for (int size : sizes) {
             vector<double> a(size, 1.0);
             vector<double> b(size, 2.0);
             vector<double> c(size);
